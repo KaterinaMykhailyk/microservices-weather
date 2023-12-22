8ac908c5e7f7bc74c8beccb269ebcba0ec549e8c
kmykhailyk <kateryna.mykhailyk@dataart.com>
2023-12-21 22:04:50 -0800

    Set up SQL Server DB for Precipitation Microservice, add docker-compose.yml

---
+++ CloudWeather.Precipitation/DataAccess/Precipitation.cs
@@ -0,0 +1,10 @@
+namespace CloudWeather.Precipitation.DataAccess;
+
+public class Precipitation
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedOn { get; set; }
+    public decimal AmountInches { get; set; }
+    public required string WeatherType { get; set; }
+    public required string ZipCode { get; set; }
+}
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,12 +1,35 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<PrecipDBContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseSqlServer(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipDBContext db) =>
 {
-    return Results.Ok(zip);
+    if (days == null || days < 0 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query parameter between 1 and 30");
+    }
+    
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+
+    PrecipDBContext context = new PrecipDBContext();
+    var result = await db.Precipitation
+        .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+        .ToListAsync();
+    
+    return Results.Ok(result);
 });
 
 app.Run();
