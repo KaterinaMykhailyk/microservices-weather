64481fefafaa68722bd940c8bb608c1b0bacbe39
kmykhailyk <kateryna.mykhailyk@dataart.com>
2023-12-30 18:23:20 -0800

    Inject MySQL DbContext in WeatherReport Microservice

--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<WeatherReportDBContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseMySQL(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 
 var app = builder.Build();
 
