2e8af2c1be5e6ff7bd2b2b4f445490ba7d1f36a9
kmykhailyk <kateryna.mykhailyk@dataart.com>
2023-12-30 18:01:41 -0800

    Set up MySQL for WeatherReport Microservice

---
+++ CloudWeather.Report/DataAccess/WeatherReportDBContext.cs
@@ -0,0 +1,22 @@
+using CloudWeather.Report.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class WeatherReportDBContext : DbContext
+{
+    public WeatherReportDBContext() {}
+
+    public WeatherReportDBContext(DbContextOptions opts) : base(opts) {}
+    
+    public DbSet<WeatherReport> WeatherReports { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        SnakeCaseIdentityTableNames(modelBuilder);
+    }
+
+    private static void SnakeCaseIdentityTableNames(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<WeatherReport>(b => { b.ToTable("weather_report"); });
+    }
+}
