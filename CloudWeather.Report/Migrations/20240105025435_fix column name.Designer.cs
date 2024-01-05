6d93cbe5d592f7cca90b656be5d126d4f65e09ec
kmykhailyk <kateryna.mykhailyk@dataart.com>
2024-01-04 19:01:18 -0800

    Fix column name in WeatherReport Microservice Db

--- CloudWeather.Report/Migrations/20240105025435_fix column name.Designer.cs
+++ CloudWeather.Report/Migrations/20240105025435_fix column name.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CloudWeather.Report.Migrations
 {
     [DbContext(typeof(WeatherReportDBContext))]
-    partial class WeatherReportDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240105025435_fix column name")]
+    partial class fixcolumnname
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CloudWeather.Report/Migrations/20240105025435_fix column name.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudWeather.Report.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixcolumnname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShowTotalInches",
+                table: "weather_report",
+                newName: "SnowTotalInches");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SnowTotalInches",
+                table: "weather_report",
+                newName: "ShowTotalInches");
+        }
+    }
+}
